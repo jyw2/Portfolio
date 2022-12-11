7c4adc3d3e13555c55f4b7a50cb23fc066144b6f
josh <jyw2@ualberta.ca>
2022-12-10 20:11:16 -0700

    Fix image API

--- src/app/gallery/gallery-menu/gallery-menu.component.ts
+++ src/app/gallery/gallery-menu/gallery-menu.component.ts
@@ -20,47 +20,47 @@ export class GalleryMenuComponent implements OnInit {
   //so Runtime is ok.
 
   //associates a name to the tag code
-  public filterData:{name:string, tag:string}[] = [];
+  public filterData: { name: string, tag: string }[] = [];
 
   //communication vars
   @Output() public apiCall = new EventEmitter<{}>() // connects to
   //menu and tells the menu component to re-render the images
 
   //collapsing bar vars
-  public open:boolean = true;
+  public open: boolean = true;
   public collapse = false;
-  public screenSizeSub:Subscription = new Subscription;
-  public screenSize:number = 0;
+  public screenSizeSub: Subscription = new Subscription;
+  public screenSize: number = 0;
 
-  constructor( private httpClient: HttpClient,private mQs: MediaQueriesService) { }
+  constructor(private httpClient: HttpClient, private mQs: MediaQueriesService) { }
 
   ngOnInit(): void {
 
     this.filterData.push(
       //Add the filters
-      {name: 'ILLUSTRATION', tag: 'illustration' },
-      {name: 'CHARACTER DESIGN', tag: 'charSheet' },
-      {name: '3D', tag: 'model' },
-      {name: 'CONCEPT ART', tag: 'concept' },
-      {name: 'TRADITIONAL', tag: 'trad' }
+      { name: 'ILLUSTRATION', tag: 'illustration' },
+      { name: 'CHARACTER DESIGN', tag: 'charSheet' },
+      { name: '3D', tag: 'model' },
+      { name: 'CONCEPT ART', tag: 'concept' },
+      { name: 'TRADITIONAL', tag: 'trad' }
     )
 
-    for (let filter of this.filterData ){
+    for (let filter of this.filterData) {
       //add all tags initially
       this.filters.push(filter.tag)
     }
-    this.filters.push('splashArt', 'wallpaper','inDepth')
+    this.filters.push('splashArt', 'wallpaper', 'inDepth')
     this.populateImages()
 
 
     //collapsing bar logic
 
     //gets screen size information
-    this.screenSizeSub = this.mQs.getQueries().subscribe((num)=>{
-      if(num> 0){
+    this.screenSizeSub = this.mQs.getQueries().subscribe((num) => {
+      if (num > 0) {
         this.open = false
 
-      }else{
+      } else {
         this.open = true
       }
       this.screenSize = num;
@@ -70,48 +70,48 @@ export class GalleryMenuComponent implements OnInit {
 
   }
 
-  toggle(){
+  toggle() {
     //for opening collapsing bar
-    if (this.open){
+    if (this.open) {
 
       this.open = false
-    }else {
+    } else {
 
       this.open = true
     }
   }
 
-  editFilters(filterData:any){
+  editFilters(filterData: any) {
     //adds or removes a tag from the filters to send to the API
 
-    if(filterData.isSelected){
+    if (filterData.isSelected) {
       //add a filter
       this.filters.push(filterData.tag)
 
       // illustration selected
-      if(filterData.tag == 'illustration'){
+      if (filterData.tag == 'illustration') {
         this.filters.push('splashArt', 'wallpaper')
-      }else if (filterData.tag == 'charSheet'){
+      } else if (filterData.tag == 'charSheet') {
         //design selected
         this.filters.push('inDepth')
       }
 
-    }else{
+    } else {
       //remove a filter
       let index = this.filters.indexOf(filterData.tag)
-      this.filters.splice(index,1)
+      this.filters.splice(index, 1)
 
       //illustration selected
-      if(filterData.tag == 'illustration'){
+      if (filterData.tag == 'illustration') {
         index = this.filters.indexOf('splashArt')
-        this.filters.splice(index,1)
+        this.filters.splice(index, 1)
 
         index = this.filters.indexOf('wallpaper')
-        this.filters.splice(index,1)
-      }else if (filterData.tag == 'charSheet'){
+        this.filters.splice(index, 1)
+      } else if (filterData.tag == 'charSheet') {
         //design selected
         index = this.filters.indexOf('inDepth')
-        this.filters.splice(index,1)
+        this.filters.splice(index, 1)
       }
 
     }
@@ -121,10 +121,10 @@ export class GalleryMenuComponent implements OnInit {
     //API uses query strings, one for each tag
     let key = 0
     let querySt = '?'
-    for (let tag of this.filters ){
+    for (let tag of this.filters) {
       querySt += `${key++}=${tag}&`
     }
-    this.httpClient.get('https://api.jyuenw.com'+ querySt).subscribe((images)=>{
+    this.httpClient.get('https://jyuenwapi.azurewebsites.net' + querySt).subscribe((images) => {
       //send Images to the main gallery component to render
       this.apiCall.emit(images)
     })
@@ -133,9 +133,9 @@ export class GalleryMenuComponent implements OnInit {
 
   }
 
-  populateImages(){
+  populateImages() {
     //used on first page load to load all images
-    this.httpClient.get('https://api.jyuenw.com/all').subscribe((images)=>{
+    this.httpClient.get('https://jyuenwapi.azurewebsites.net/all').subscribe((images) => {
       //send Images to the main gallery component to render
       this.apiCall.emit(images)
     })
